1177f71fcf5c40520d938aa4a9791299ec003427
Vinicius Tessari <111604185+Vinicius-Tessari@users.noreply.github.com>
2025-11-13 19:59:37 -0400

    13-11
    ATUALIZAÇÃO

--- acervo/urls.py
+++ acervo/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('itens/', views.ItemListView.as_view(), name='itens'),
+    path('emprestimos/', views.EmprestimoListView.as_view(), name='emprestimos'),
+    path('emprestar/', views.emprestimo_create, name='emprestar'),
 ]
 
--- acervo/views.py
+++ acervo/views.py
@@ -9,3 +9,26 @@ from .forms import EmprestimoForm
 def home(request):
     return render(request, 'acervo/home.html')
 
+class ItemListView(ListView):
+    template_name = 'acervo/item_list.html'
+    context_object_name = 'itens'
+
+    def get_queryset(self):
+        return list(Livro.objects.all()) + list(Revista.objects.all()) + list(MidiaDigital.objects.all())
+    
+class EmprestimoListView(ListView):
+    model = Emprestimo
+    template_name = 'acervo/emprestimo_list.html'
+    context_object_name = 'emprestimos'
+
+def emprestimo_create(request):
+    if request.method == 'POST':
+        form = EmprestimoForm(request.POST)
+        if form.is_valid():
+            emp = form.save
+            messages.sucess(request, f'Empréstimo criado: {emp}')
+            return redirect ('emprestimos')
+    else:
+        form = EmprestimoForm()
+    return render(request, 'acervo/emprestimo_form.html', {'form': form})
+
